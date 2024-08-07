8fce98e2824bf36069116bbc844fd22adb910fb7
Laiba-Faisal <nooreasal786@gmail.com>
2024-08-07 20:48:52 +0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,7 @@
+n1 = eval(input("Enter the first number: "))
+operator = input("Enter the operator: ")
+n2 = eval(input("Enter the second number: "))
+# operator_lis = ["+", "-", "*", "/"]
+# if operator in operator_lis:
+print("The result is: ", eval(f"{n1}{operator}{n2}"))
+
